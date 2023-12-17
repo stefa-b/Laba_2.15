5cc5c3b558c59608dd86bfaf0ac99992e91f0441
Stefania <126661019+stefa-b@users.noreply.github.com>
2023-12-18 01:22:46 +0300

    Add files via upload

---
+++ Пример 11.py
@@ -0,0 +1,7 @@
+#Приведенный выше код создаст каталог new в текущей рабочей папке.
+
+if __name__ == "__main__":
+    import os
+
+    # creating a new directory with the name new
+    os.mkdir("new")
